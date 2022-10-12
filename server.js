6f8ba03724cf651d7740bd88a4d07258f8929c2b
Reece Devenney <reecedevenney@gmail.com>
2022-10-12 18:38:47 -0400

    added login and logout

--- server.js
+++ server.js
@@ -4,11 +4,22 @@ const sequelize = require('./config/connection.js')
 const path = require('path')
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-
+const sess = {
+  secret: process.env.COOKIE_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
