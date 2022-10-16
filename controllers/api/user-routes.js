cec9869975dfd1b0c4bb1ecf821c58f27a0a738f
Reece Devenney <reecedevenney@gmail.com>
2022-10-16 12:31:57 -0400

    added user dashboard

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Vote, Comment } = require("../../models");
+const withAuth = require('../../utils/auth');
 
 
 // GET /api/users
@@ -111,7 +112,7 @@ router.post('/login', (req, res) => {
     });
 })
 
-router.post('/logout', (req, res) => {
+router.post('/logout', withAuth, (req, res) => {
     if (req.session.loggedIn) {
         req.session.destroy(() => {
             res.status(204).end()
@@ -123,7 +124,7 @@ router.post('/logout', (req, res) => {
 
 
 // PUT /api/users/1
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
     // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
@@ -148,7 +149,7 @@ router.put('/:id', (req, res) => {
 
 
 // DELETE /api/users/1
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     User.destroy({
         where: {
             id: req.params.id
